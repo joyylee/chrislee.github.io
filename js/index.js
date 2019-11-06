40fbf71182b80f3719ea977245edd866fa0433b3
J Lee <jlee@0587403185.wireless.umich.net>
2019-11-06 12:06:07 -0500

    hello

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 var i = 0;
 var txt = 'Scroll down to learn more about me!'; /* The text */
-var speed = 40; /* The speed/duration of the effect in milliseconds */
+var speed = 65; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
     if (i < txt.length) {
