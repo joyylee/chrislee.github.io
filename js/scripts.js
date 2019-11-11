45fcfff786bb42d4399bec09da7719cc7b07b27e
J Lee <jlee@0587404479.wireless.umich.net>
2019-11-11 11:28:47 -0500

    hello

--- js/scripts.js
+++ js/scripts.js
@@ -15,33 +15,58 @@ function closeModal() {
 }
 
 var slideIndex = 1;
+var captionsIndex = 1;
+
 showSlides(slideIndex);
+showCaptions(captionsIndex);
 
 // Next/previous controls
 function plusSlides(n) {
     showSlides(slideIndex += n);
+    showCaptions(captionsIndex += n);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
     showSlides(slideIndex = n);
+    showCaptions(captionsIndex = n);
 }
 
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
+    var captions = document.getElementsByClassName("demo");
     var captionText = document.getElementById("caption");
+
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    // for (i = 0; i < dots.length; i++) {
-    //     dots[i].className = dots[i].class.replace(" active", "");
-    // }
     slides[slideIndex - 1].style.display = "block";
-    // dots[slideIndex - 1].class += " active";
-    captionText.innerHTML = dots[slideIndex - 1].alt;
-    console.log(dots[slideIndex - 1].alt);
+
+
+    // captionText.innerHTML = captions[captionsIndex - 1].alt;
+    // console.log(captions[captionsIndex - 1].alt);
+}
+
+
+
+// Next/previous controls
+function plusCaptions(n) {
+    showCaptions(captionsIndex += n);
+}
+
+function showCaptions(n) {
+    var i;
+    // var slides = document.getElementsByClassName("mySlides");
+    var captions = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+
+    if (n > captions.length) { captionsIndex = 1 }
+    if (n < 1) { captionsIndex = captions.length }
+    for (i = 0; i < captions.length; i++) {
+        captions[i].style.display = "none";
+    }
+    captions[captionsIndex - 1].style.display = "block";
 }
